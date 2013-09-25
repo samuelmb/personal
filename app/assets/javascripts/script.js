5dc2b338c88131ce64ad03e2fec4bfebbd1e3320
Sam Balinghasay <samuel.balinghasay@gmail.com>
2013-09-24 20:52:16 -0700

    add buffer to project panels end

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,12 +6,12 @@ $(function() {
 
   $('a#resume').on("click", function(event) {
     event.preventDefault();
-    $(window).scrollTo({top: 4000, left: 0}, 800);
+    $(window).scrollTo({top: 4500, left: 0}, 800);
   });
 
   $('a#contact').on("click", function(event) {
     event.preventDefault();
-    $(window).scrollTo({top: 6000, left: 0}, 800);
+    $(window).scrollTo({top: 6500, left: 0}, 800);
   });
 
 });
